a50029da2e263cf7a9adf3c9155531ae46aed1f3
Anastasiia Makarenko <irishmoth@yandex.ru>
2025-06-27 17:07:49 +0300

    добавляю AppNavigation, AppDrawer

---
+++ app/src/main/java/pro/progr/saveanowl/AppDrawer.kt
@@ -0,0 +1,38 @@
+package pro.progr.saveanowl
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.DrawerState
+import androidx.compose.material.ModalDrawer
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import pro.progr.fallingdiamonds.composable.SundukDrawerWidget
+import pro.progr.owlgame.presentation.ui.WidgetScreen
+import pro.progr.todos.DiamondViewModel
+
+@Composable
+fun AppDrawer(drawerState: DrawerState,
+              viewModel: DiamondViewModel,
+              navController: NavHostController,
+              content : @Composable () -> Unit) {
+
+    ModalDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            Column(modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 100.dp)) {
+                SundukDrawerWidget(
+                    diamondsTotal = viewModel.getDiamondsCount().collectAsState(initial = 0),
+                    {navController.navigate("sunduk")})
+                WidgetScreen(navController)
+            }
+
+        },
+        content = content
+    )
+}
---
+++ app/src/main/java/pro/progr/saveanowl/AppNavigation.kt
@@ -0,0 +1,63 @@
+package pro.progr.saveanowl
+
+import androidx.compose.material.DrawerValue
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import pro.progr.owlgame.presentation.navigation.OwlNavigation
+import pro.progr.todos.DiamondViewModel
+import pro.progr.todos.DiamondsCountRepository
+import pro.progr.todos.TodosNavigation
+
+@OptIn(ExperimentalMaterialApi::class)
+@kotlinx.coroutines.ExperimentalCoroutinesApi
+@Composable
+fun AppNavigation(diamondViewModel: DiamondViewModel, diamondsCountRepository: DiamondsCountRepository) {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "todos") {
+        composable("todos") {
+            val drawerState = rememberDrawerState(DrawerValue.Closed)
+            TodosNavigation(
+                appDrawer = { a, b, c, d -> AppDrawer(a, b, c, d) },
+                diamondViewModel = diamondViewModel,
+                navController = navController,
+                drawerState = drawerState
+            )
+        }
+
+        //Навигация в модуле "Спаси сову"
+        composable("owl_navigation") {
+            OwlNavigation (diamondDao = diamondsCountRepository,
+                backToMain = { navController.popBackStack()})
+        }
+
+        //Навигация в модуле "Спаси сову"
+        composable("owl_navigation/pouch") {
+            OwlNavigation(startDestination = "pouch", diamondDao = diamondsCountRepository,
+                backToMain = { navController.popBackStack() })
+        }
+
+        //Навигация в модуле "Спаси сову"
+        composable(
+            route = "animal?id={id}",
+            arguments = listOf(navArgument("id") {
+                type = NavType.StringType
+                nullable = true
+                defaultValue = null
+            })
+        ) { backStackEntry ->
+            val id = backStackEntry.arguments?.getString("id")
+
+            OwlNavigation(startDestination = "animal_searching/$id", diamondDao = diamondsCountRepository,
+                backToMain = { navController.navigate("calendar") })
+
+        }
+
+    }
+}
