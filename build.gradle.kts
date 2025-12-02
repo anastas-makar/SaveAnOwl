167381978b575ae6aad26fed5f75eb6c74e50021
Anastasiia Makarenko <irishmoth@yandex.ru>
2025-12-02 14:07:50 +0300

    версии

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.13.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
-    id("com.google.devtools.ksp") version "1.9.25-1.0.20" apply false
+    id("com.android.application") version "8.13.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
+    id("com.google.devtools.ksp") version "2.2.21-2.0.4" apply false
     id("vkid.manifest.placeholders") version "1.1.0" apply true
 }
 
